f3a746803c988ab7be5a2296d1a77a42c879bb6f
unknown <szymon.tkacz.it@gmail.com>
2021-03-05 01:59:01 +0100

    quick fix

--- Models/HousingLoan.cs
+++ Models/HousingLoan.cs
@@ -1,9 +1,5 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Banqsoft.Models
 {
