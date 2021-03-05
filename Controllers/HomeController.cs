ed9a317093e25435098f8eeb2ad5357b2d37672f
unknown <szymon.tkacz.it@gmail.com>
2021-03-05 01:44:20 +0100

    Upload Project

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Banqsoft.Models;
+
+namespace Banqsoft.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Loan(HousingLoan housingLoan)
+        {
+            housingLoan.TotalCost = Math.Round(housingLoan.Amount * Math.Pow(1 +((housingLoan.Rate / 100)), housingLoan.PaybackTime), 2);
+            housingLoan.MonthlyTotal = Math.Round(housingLoan.TotalCost / (housingLoan.PaybackTime * 12), 2);
+            housingLoan.MonthlyPayback = Math.Round(housingLoan.Amount / (housingLoan.PaybackTime * 12), 2);
+            housingLoan.MonthlyInterest = Math.Round(housingLoan.MonthlyTotal - housingLoan.MonthlyPayback, 2);
+            return View(housingLoan);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/HousingLoan.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Banqsoft.Models
+{
+    public class HousingLoan
+    {
+        public double Rate = 3.5;
+
+        [Required]
+        [Range(10000, 300000)]
+        [DataType(DataType.Currency)]
+        public double Amount { get; set; }
+
+        [Required]
+        [Range(5, 40)]
+        public int PaybackTime { get; set; }
+        public double MonthlyPayback { get; set; }
+        public double MonthlyInterest { get; set; }
+        public double MonthlyTotal { get; set; }
+        public double TotalCost { get; set; }
+    }
+}
